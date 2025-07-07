4eb0ed001c1529e316ff6682315fb2b462ffba7c
Vinsmoke Somya <vinsmokesomya@gmail.com>
2025-07-07 21:17:12 +0530

    Update site URL in Astro config

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,7 +5,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO: Replace `<YOUR-GITHUB-USERNAME>` with your actual GitHub username.
   site: 'https://VinsmokeSomya.github.io',
   base: '/RabbitHole',
   integrations: [svelte()]
